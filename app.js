f123bc68b7658f838d9d2be46987b9d6527a5f5e
Alberto <a>
2017-07-24 19:58:32 -0500

    chat layout

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var app = require('express')()
+var express = require('express');
+var path = require('path');
+
+var app = express()
 var http = require('http').createServer(app)
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html')
 })
-
-http.listen(PORT || 3000 , function(){
-	console.log('Escuchando en puerto 3000')
+let port = (process.env.PORT || 3000)
+http.listen(port, function(){
+	console.log('Escuchando en puerto ' + port)
 })
